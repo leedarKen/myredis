2c97e6806abfce99eceefaa3048d201d3dcad940
zfkang <348999853@qq.com>
2018-07-17 17:25:07 +0800

    add source

---
+++ src/test/java/org/mytest/AppTest.java
@@ -0,0 +1,20 @@
+package org.cbtest;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
---
+++ src/test/java/org/mytest/FirstJUnit5Tests.java
@@ -0,0 +1,13 @@
+package org.cbtest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FirstJUnit5Tests {
+
+    @Test
+    void myFirstTest() {
+        assertEquals(2, 1 + 1);
+    }
+}
